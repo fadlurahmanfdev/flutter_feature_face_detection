9cb387d39d4b55fc7dd7167c35beaf46f66c4289
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2024-09-17 23:58:18 +0700

    fix(publish): fix dependencies

--- lib/src/domain/plugin/flutter_face_detection.dart
+++ lib/src/domain/plugin/flutter_face_detection.dart
@@ -2,8 +2,8 @@ import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/services.dart';
-import 'package:flutter_feature_face_detection/flutter_feature_face_detection.dart';
-import 'package:camera/camera.dart';
+import 'package:flutter_feature_camera/camera.dart';
+import 'package:flutter_feature_face_detection/src/domain/extension/camera_image_extension.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 
 class FlutterFaceDetection {
